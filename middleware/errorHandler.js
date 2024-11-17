a34f5fca89c4e4a2d3500ca1f4def1c5de5d4f95
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2024-11-17 23:49:12 +0530

    updated_errorHandler_DuplicateFiledIdentification_Dt:17112024

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -5,11 +5,17 @@ const errorHandler = (err, req, res, next) => {
 
   error.message = err.message;
 
-  console.log(err.stack.red);
+  console.log(err);
 
   //   if the error is CastError then the following activity is to be performed
   if (err.name === "CastError") {
     const message = `Resource not found for the id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  // duplication error handler
+  if (err.code === 11000) {
+    const message = "Duplicate field value entered";
     error = new ErrorResponse(message, 404);
   }
 
