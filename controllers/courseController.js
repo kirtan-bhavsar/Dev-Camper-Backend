d484cac32a5484705a3f2f059e9dd01cf2664b5e
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2024-11-27 00:50:18 +0530

    added_courseController&courseRoutes_getCourseById_Dt:27112024

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,10 +1,11 @@
 import expressAsyncHandler from "express-async-handler";
 import Course from "../models/Course.js";
+import asyncHandler from "express-async-handler";
 
 // @desc get all course or get courses of specific boocamp
 // @api GET api/v1/courses or GET api/v1/bootcamps/:bootcampId/courses
 // @access public
-const getCourses = expressAsyncHandler(async (req, res, next) => {
+const getCourses = asyncHandler(async (req, res, next) => {
   let query;
 
   if (req.params.bootcampId) {
@@ -25,4 +26,24 @@ const getCourses = expressAsyncHandler(async (req, res, next) => {
   });
 });
 
-export { getCourses };
+// @desc get all course or get courses of specific boocamp
+// @api GET api/v1/courses or GET api/v1/bootcamps/:bootcampId/courses
+// @access public
+const getCourseById = asyncHandler(async (req, res, next) => {
+  const courseId = req.params.id;
+
+  const course = await Course.findById(courseId);
+
+  if (!course) {
+    return next(
+      new ErrorResponsez(`No course found for the id : ${courseId}`, 404)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+export { getCourses, getCourseById };
--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,10 +1,11 @@
 import mongoose, { get } from "mongoose";
-import { getCourses } from "../controllers/courseController.js";
+import { getCourseById, getCourses } from "../controllers/courseController.js";
 import express from "express";
 
 // here we add the mergeParams:true to enable cross API functionality
 const courseRouter = express.Router({ mergeParams: true });
 
 courseRouter.get("/", getCourses);
+courseRouter.get("/:id", getCourseById);
 
 export { courseRouter as default };
